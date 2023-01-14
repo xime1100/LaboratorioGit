1640128b5cd29d26384cbfa07ba2ceca7e336d21
marpermon <maria.perezmonge@ucr.ac.cr>
2023-01-14 14:17:16 -0600

    included gato2 and list printing

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -5,3 +5,11 @@ gato1.nombre = "Sussy"
 gato1.color = "negro"
 gato1.edad = 4
 gato1.aniadirlista()
+
+gato2 = funcionesTarea.gato()
+gato2.nombre = "Pelusa"
+gato2.color = "cafe"
+gato2.edad = 3
+gato2.aniadirlista()
+
+funcionesTarea.printlista()
